f2f10057a9ee3fa31dd273ae326a7859b3b95652
unknown <leblancmichael94@gmail.com>
2022-02-07 19:37:52 -0500

    adding pricing to items objects

--- src/items.js
+++ src/items.js
@@ -2,53 +2,63 @@ import alienware from './alienware.avif';
 
 export const items = [
  {   id: 1,
+     category: "PC", 
      image: {alienware},
      name: "Alienware Aurora R13 Gaming PC - 1 TB 7200RPM 256GB NVMe SSD - 16GB Dual Channel DDR5 - NVIDIA GeForce 3060 Ti 8GB",
      cost: "$2,192.41"
  },
  {   id: 2,
+    category: "PC",
     image: {},
     name: "Digital Storm Lumos Gaming PC - 2TB 7200RPM Storage - 16GB DDR4 Memory - NVIDIA Graphics",
     cost: "$1,672.00"
 },
 {   id: 3,
+    category: "PC",
     image: {},
     name: "Legion Tower 5 (AMD) - 1TB SSD Storage RTX 3060 - NVIDIA® GeForce® RTX™ 3060 12GB",
     cost: "$1,629.99"
 },
 {   id: 4,
+    category: 'Keyboard',
     image: {},
-    name: "",
-    cost: "$3,640.67"
+    name: "Alienware RGB Mechanical Gaming Keyboard AW410K",
+    cost: "$129.99"
 },
 {   id: 5,
+    category: 'Keyboard',
     image: {},
-    name: "",
-    cost: "$3,640.67"
+    name: "HyperX Alloy Origins - Mechanical Gaming Keyboard",
+    cost: "$69.99"
 },
 {   id: 6,
+    category: 'Keyboard',
     image: {},
-    name: "",
-    cost: "$3,640.67"
+    name: "Razer Huntsman Optical Switch Gaming Keyboard",
+    cost: "$151.99"
 },
 {   id: 7,
+    category: 'Mouse',
     image: {},
-    name: "",
-    cost: "$3,640.67"
+    name: "G502 LIGHTSPEED WIRELESS GAMING MOUSE",
+    cost: "$149.99"
 },
 {   id: 8,
+    category: 'Mouse',
     image: {},
-    name: "",
-    cost: "$3,640.67"
+    name: "Aerox 3 Wireless 2022 Edition",
+    cost: "$99.99"
 },
 {   id: 9,
+    category: 'Headset',
     image: {},
-    name: "",
-    cost: "$3,640.67"
+    name: "Arctis Pro Peerless High Resolution PC Gaming Headset",
+    cost: "$148.99"
 },
 {   id: 10,
+    category: 'Headset',
     image: {},
-    name: "",
-    cost: "$3,640.67"
+    name: "HyperX Cloud Flight - Wireless Gaming Headset",
+    cost: "$139.99"
 },
 ]

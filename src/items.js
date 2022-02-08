f3c6e58e693a7529d6fe41bfa31b14c83974c811
unknown <leblancmichael94@gmail.com>
2022-02-08 00:37:34 -0500

    all pictures added for the products in my store

--- src/items.js
+++ src/items.js
@@ -1,4 +1,14 @@
 import alienware from './alienware.avif';
+import digitalStorm from './digitalStorm.png';
+import legionTower from './legionTower.jpg';
+import alienwareKeyboard from './alienwareKeyboard.webp';
+import hyperX from './hyperXKeyboard.webp';
+import razorHuntsman from './razorHuntsman.png';
+import gamingMouse1 from './gamingMouse1.jpg';
+import gamingMouse2 from './gamingMouse2.webp';
+import gamingHeadset1 from './gamingHeadset1.jpg';
+import gamingHeadset2 from './gamingHeadset2.jpg';
+
 
 export const items = [
  {   id: 1,
@@ -9,55 +19,55 @@ export const items = [
  },
  {   id: 2,
     category: "PC",
-    image: {},
+    image: {digitalStorm},
     name: "Digital Storm Lumos Gaming PC - 2TB 7200RPM Storage - 16GB DDR4 Memory - NVIDIA Graphics",
     cost: "$1,672.00"
 },
 {   id: 3,
     category: "PC",
-    image: {},
+    image: {legionTower},
     name: "Legion Tower 5 (AMD) - 1TB SSD Storage RTX 3060 - NVIDIA® GeForce® RTX™ 3060 12GB",
     cost: "$1,629.99"
 },
 {   id: 4,
     category: 'Keyboard',
-    image: {},
+    image: {alienwareKeyboard},
     name: "Alienware RGB Mechanical Gaming Keyboard AW410K",
     cost: "$129.99"
 },
 {   id: 5,
     category: 'Keyboard',
-    image: {},
+    image: {hyperX},
     name: "HyperX Alloy Origins - Mechanical Gaming Keyboard",
     cost: "$69.99"
 },
 {   id: 6,
     category: 'Keyboard',
-    image: {},
+    image: {razorHuntsman},
     name: "Razer Huntsman Optical Switch Gaming Keyboard",
     cost: "$151.99"
 },
 {   id: 7,
     category: 'Mouse',
-    image: {},
+    image: {gamingMouse1},
     name: "G502 LIGHTSPEED WIRELESS GAMING MOUSE",
     cost: "$149.99"
 },
 {   id: 8,
     category: 'Mouse',
-    image: {},
+    image: {gamingMouse2},
     name: "Aerox 3 Wireless 2022 Edition",
     cost: "$99.99"
 },
 {   id: 9,
     category: 'Headset',
-    image: {},
+    image: {gamingHeadset1},
     name: "Arctis Pro Peerless High Resolution PC Gaming Headset",
     cost: "$148.99"
 },
 {   id: 10,
     category: 'Headset',
-    image: {},
+    image: {gamingHeadset2},
     name: "HyperX Cloud Flight - Wireless Gaming Headset",
     cost: "$139.99"
 },
